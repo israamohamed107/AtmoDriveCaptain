86d765fe386cad28e638d6284a6bea4c6f9b157f
Israa Mohamed <esraamoh1072000@gmail.com>
2023-08-06 01:46:04 +0300

    initial commit

---
+++ app/src/main/java/com/israa/atmodrivecaptain/ui/auth/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.israa.atmodrivecaptain.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.israa.atmodrivecaptain.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
